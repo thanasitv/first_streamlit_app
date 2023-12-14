40a693727ec5bceb5e06543a7ed101f9b54e8a80
thanasitv <31183618+thanasitv@users.noreply.github.com>
2023-12-14 13:25:44 +0700

    Update streamlit_app.py
    test # import urllib

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib
+# import urllib
 from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Dinner')
